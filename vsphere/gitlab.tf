9ee0ed23c51050aaf5e0c1da2352fc59952676ed
Andrei Costescu <andrei@costescu.no>
2021-11-08 23:05:44 +0100

    Add GitLab

---
+++ vsphere/gitlab.tf
@@ -0,0 +1,63 @@
+resource "vsphere_folder" "gitlab" {
+  path          = "GitLab"
+  type          = "vm"
+  datacenter_id = "${data.vsphere_datacenter.home.id}"
+}
+
+resource "vsphere_tag" "gitlab" {
+  name        = "gitlab"
+  category_id = "${vsphere_tag_category.ansible_group.id}"
+  description = "Gitlab servers"
+}
+
+resource "vsphere_tag" "gitrun" {
+  name        = "gitrun"
+  category_id = "${vsphere_tag_category.ansible_group.id}"
+  description = "Gitlab runners"
+}
+
+module "gitlab" {
+  source = "./modules/vm_from_tmpl"
+
+  datacenter_id    = "${data.vsphere_datacenter.home.id}"
+  datastore_id     = "${data.vsphere_datastore.tn_ssd.id}"
+  network_id       = "${data.vsphere_network.vlan10.id}"
+  resource_pool_id = "${data.vsphere_resource_pool.home.id}"
+  template_name    = "templates/rocky_packer"
+  vm_net           = "${var.vm_net_space}"
+
+  cores  = 4
+  memory = 8192
+
+  name       = "gitlab01"
+  folder     = "GitLab"
+  tags       = ["${vsphere_tag.gitlab.id}"]
+  ip_address = 40
+
+  os_disk_size   = 20
+  data_disks     = 1
+  data_disk_size = 30
+}
+
+module "gitrun" {
+  source = "./modules/vm_from_tmpl"
+
+  datacenter_id    = "${data.vsphere_datacenter.home.id}"
+  datastore_id     = "${data.vsphere_datastore.tn_ssd.id}"
+  network_id       = "${data.vsphere_network.vlan10.id}"
+  resource_pool_id = "${data.vsphere_resource_pool.home.id}"
+  template_name    = "templates/rocky_packer"
+  vm_net           = "${var.vm_net_space}"
+
+  cores  = 4
+  memory = 4096
+
+  count      = 2
+  name       = format("%s%02s", "gitrun", count.index + 1)
+  folder     = "GitLab"
+  tags       = ["${vsphere_tag.gitrun.id}"]
+  ip_address = 40 + count.index + 1
+
+  data_disks     = 1
+  data_disk_size = 20
+}
