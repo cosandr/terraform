1f6c298d5e1c655907d382036db6aba66d391283
Andrei Costescu <andrei@costescu.no>
2021-11-08 22:27:25 +0100

    Change cardinality of ansible_group

--- vsphere/tag_categories.tf
+++ vsphere/tag_categories.tf
@@ -1,6 +1,6 @@
 resource "vsphere_tag_category" "ansible_group" {
   name        = "ansible_group"
-  cardinality = "SINGLE"
+  cardinality = "MULTIPLE"
   description = "Tags read by Ansible"
 
   associable_types = ["VirtualMachine"]
