d8290e8399f1f1118c8876d0be13acfdce821085
Andrei Costescu <andrei@costescu.no>
2022-01-22 13:51:22 +0100

    Add Plex

---
+++ vsphere/plex.tf
@@ -0,0 +1,27 @@
+resource "vsphere_tag" "plex" {
+  name        = "plex"
+  category_id = "${vsphere_tag_category.ansible_group.id}"
+  description = "Plex servers"
+}
+
+module "plex" {
+  source = "./modules/vm_from_tmpl"
+
+  datacenter_id    = "${data.vsphere_datacenter.home.id}"
+  datastore_id     = "${data.vsphere_datastore.vm.id}"
+  network_id       = "${vsphere_distributed_port_group.vm.id}"
+  resource_pool_id = "${data.vsphere_resource_pool.home.id}"
+  template_name    = "templates/rocky_packer"
+  vm_net           = "${var.vm_net_space}"
+
+  cores  = 12
+  memory = 10240
+
+  name       = "plex01"
+  tags       = ["${vsphere_tag.plex.id}"]
+  ip_address = 65
+
+  os_disk_size   = 20
+  data_disks     = 1
+  data_disk_size = 50
+}
