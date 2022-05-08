ab1266e6285db51c6d91ae8f97144c2ac7ca76d6
Andrei Costescu <andrei@costescu.no>
2022-05-08 21:17:34 +0200

    Split Kubernetes into master and worker

--- vsphere/kube.tf
+++ vsphere/kube.tf
@@ -10,7 +10,7 @@ resource "vsphere_tag" "kube" {
   description = "Kubernetes servers"
 }
 
-module "kube" {
+module "kube_master" {
   source = "./modules/vm_from_tmpl"
 
   datacenter_id    = "${data.vsphere_datacenter.home.id}"
@@ -21,6 +21,31 @@ module "kube" {
   vm_net           = "${var.vm_net_space}"
 
   cores  = 4
+  memory = 4096
+
+  extra_config = {
+    "disk.EnableUUID" = "True"
+  }
+
+  name       = "kube-master"
+  folder     = "kube"
+  tags       = ["${vsphere_tag.kube.id}", "${vsphere_tag.autostart.id}"]
+  ip_address = 50
+
+  os_disk_size   = 20
+}
+
+module "kube_worker" {
+  source = "./modules/vm_from_tmpl"
+
+  datacenter_id    = "${data.vsphere_datacenter.home.id}"
+  datastore_id     = "${data.vsphere_datastore.vm.id}"
+  network_id       = "${vsphere_distributed_port_group.vm.id}"
+  resource_pool_id = "${data.vsphere_resource_pool.home.id}"
+  template_name    = "templates/rocky_packer"
+  vm_net           = "${var.vm_net_space}"
+
+  cores  = 8
   memory = 8192
 
   extra_config = {
@@ -28,7 +53,7 @@ module "kube" {
   }
 
   count      = 3
-  name       = format("%s%02s", "kube", count.index + 1)
+  name       = format("%s%02s", "kube-worker", count.index + 1)
   folder     = "kube"
   tags       = ["${vsphere_tag.kube.id}", "${vsphere_tag.autostart.id}"]
   ip_address = 50 + count.index + 1
