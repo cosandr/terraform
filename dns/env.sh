aa067a35d8584390e3ac5910ae92fc93d205ab6b
Andrei Costescu <andrei@costescu.no>
2025-06-02 21:28:10 +0200

    Workaround TF bug

--- dns/env.sh
+++ dns/env.sh
@@ -7,3 +7,7 @@
 
 export AWS_ACCESS_KEY_ID="$(gopass show -o terraform/state_backblaze_id)"
 export AWS_SECRET_ACCESS_KEY="$(gopass show -o terraform/state_backblaze_secret)"
+
+# https://github.com/hashicorp/terraform/issues/36704#issuecomment-2765488717
+export AWS_REQUEST_CHECKSUM_CALCULATION=when_required
+export AWS_RESPONSE_CHECKSUM_VALIDATION=when_required
